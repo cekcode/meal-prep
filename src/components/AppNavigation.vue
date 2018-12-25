775b00b86e396a829da69fb9f8b81093c7ed3535
cekcode <cekcode@gmail.com>
2018-12-25 21:45:25 +0700

    added vuex for handle

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -16,6 +16,7 @@
             <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-btn flat class="hidden-sm-and-down" to="/">Home</v-btn>
             <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
             <v-spacer class="hidden-sm-and-down"></v-spacer>
             <v-btn flat class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await axios.get(`${state.apiurl}`, {
+                    params: {
+                        q: plan,
+                        app_id: '05efdf5f',
+                        app_key: 'df044717b68d8206a2d21324c5e93d4d',
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        }
+    }
 });
